11de754bc1284a0cdc92005967fe88fca934f9e5
Reuben Lifshay <rlifshay@gmail.com>
2025-02-28 22:57:19 -0800

    feat: add MASQUERADE to options

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,10 +54,11 @@ setup_utils () {
 
 : \
 	${DMA_MAILNAME=${MAILNAME-}} \
+	${DMA_MASQUERADE=${MASQUERADE-}} \
 	${DMA_SMARTHOST=${SMARTHOST-}} \
 	${DMA_PORT=${SMARTHOST_PORT-}} \
 	${DMA_AUTHPATH="/etc/dma/auth.conf"}
-export DMA_MAILNAME DMA_SMARTHOST DMA_PORT DMA_AUTHPATH
+export DMA_MAILNAME DMA_SMARTHOST DMA_SMARTHOST DMA_PORT DMA_AUTHPATH
 
 # set default command if flags passed
 [ "${1#-}" = "$1" ] || set -- msmtpd "$@"
